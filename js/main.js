07abe84efdcc5e46558cd0dd84dc12c5902c163f
Shelley Zhang <shelleyyz91@gmail.com>
2018-05-30 14:15:48 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// remember that JS renders code based on most recent
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; /*this will hold the cards that are in play/flipped*/
+
+var cardOne = cards[0]; //queen
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2]; //king
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again");
+}
+}
